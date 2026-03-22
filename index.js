36b2627cc6b3cfcb96d58306530d6e63f930c4dd
allisonvaldez <alleyvaldez1@outlook.com>
2026-03-21 17:14:06 -0700

    problem with submodule

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+//1.  Fetch Data from an API- Using the fetch method, fetch to {JSON} Placeholder TypicodeLinks to an external site. posts.
+
+// 2.  Display Posts- Create a function to display posts called displayPosts().
+    // Pass in the array of posts.
+    // Loop through the posts list.
+    // 3.  Within the loop:
+        // Create a li tag.
+        // Create a new h1 tag: Add the title as the textContent.
+        // Create a new p tag.
+        // Add the body as the textContent.
+        // Append h1 and p to li.
+        // Append li to the ul: The id of the ul is post-list.
+        // Call displayPosts() function after fetch.
+
+// Refactor with async/await.
+// Create a function to house fetch and apply async to the function.
+// Apply await to the return of fetch.
